d096384bbbadc747e817a43adbdb5158b5a1f1a0
pipwitheridge <phillipwitheridge@gmail.com>
2024-03-07 21:37:38 +1100

    Fixed screen css things. Added questions.

---
+++ quizQuestions.js
@@ -0,0 +1,106 @@
+var quizQuestions = [
+    {
+        "moduleName": "God our maker",
+        "questionNo": 1,
+        "question": "What do the first few chapters of Genesis aim to give us primarily?",
+        "options": ["A theological account of God and people", "A scientific treatise of the origins of matter", "An historical recount of the first humans"],
+        "answer": ["A theological account of God and people"],
+        "correctMessage": "That's right—the creation account of Genesis is primarily about providing a view of God, people and our world."
+    },
+    {
+        "moduleName": "God our maker",
+        "questionNo": 2,
+        "question": "What words accurately describe the way in which God creates in the Bible?",
+        "options": ["Lovingly", "Recklessly", "Powerfully", "Randomly"],
+        "answer": ["Lovingly", "Powerfully"],
+        "correctMessage": "Yes. That's correct."
+    },
+    {
+        "moduleName": "God our maker",
+        "questionNo": 3,
+        "question": "Which example was given to show how God's creative activity is used within evangelism in the New Testament?",
+        "options": ["Peter's speech in Acts 2", "Paul's speech in Acts 17", "The opening of John's gospel"],
+        "answer": ["Paul's speech in Acts 17"],
+        "correctMessage": "Yes—Paul describes God as the one who gives life to everyone."
+    },
+    {
+        "moduleName": "God our enemy",
+        "questionNo": 1,
+        "question": "How is sin described in the Bible?",
+        "options": ["Not loving others as we ought", "Not loving God as we ought", "Not doing the good we know we ought to do", "A universal problem"],
+        "answer": ["Not loving others as we ought", "Not loving God as we ought", "Not doing the good we know we ought to do", "A universal problem"],
+        "correctMessage": "Yes, these are all ways in which the Bible describes sin."
+    },
+    {
+        "moduleName": "God our enemy",
+        "questionNo": 2,
+        "question": "What does sin lead to?",
+        "options": ["Forgiveness", "Death", "Wealth", "Separation from God"],
+        "answer": ["Death", "Separation from God"],
+        "correctMessage": "Yes. That's correct."
+    },
+    {
+        "moduleName": "God our enemy",
+        "questionNo": 3,
+        "question": "What image does C.S. Lewis use to describe sinners?",
+        "options": ["Children in debt to their parents", "Rebels who must lay down their arms", "Dogs stuck in a great storm"],
+        "answer": ["Rebels who must lay down their arms"],
+        "correctMessage": "Yes—the Bible portrays sin's remedy as not a small moral improvement, but a total renovation of the heart."
+    },
+    {
+        "moduleName": "God our saviour",
+        "questionNo": 1,
+        "question": "What makes Jesus' our saviour?",
+        "options": ["His life", "His death", "His resurrection", "His surname"],
+        "answer": ["His life", "His death", "His resurrection"],
+        "correctMessage": "Yes, Jesus' perfect obedience, substitutionary death, and bodily resurrection are all part of his saving work."
+    },
+    {
+        "moduleName": "God our saviour",
+        "questionNo": 2,
+        "question": "How does the Bible describe salvation?",
+        "options": ["An undeserved gift", "A one-time offer", "Eternal life"],
+        "answer": ["An undeserved gift", "Eternal life"],
+        "correctMessage": "Yes. That's correct."
+    },
+    {
+        "moduleName": "God our saviour",
+        "questionNo": 3,
+        "question": "According to Jonathan Edwards, what do we contribute to our salvation?",
+        "options": ["Ongoing allegiance to Christ", "The sin that made it necessary", "Financial generosity"],
+        "answer": ["The sin that made it necessary"],
+        "correctMessage": "Yes—God is the one who accomplishes salvation for His people."
+    },
+    {
+        "moduleName": "God our saviour",
+        "questionNo": 4,
+        "question": "What does salvation lead to?",
+        "options": ["A new life in the here and now", "Eternal life", "Complacency"],
+        "answer": ["A new life in the here and now", "Eternal life"],
+        "correctMessage": "That's right. Being saved from sin impacts our lives now and forever."
+    },
+    {
+        "moduleName": "God our friend",
+        "questionNo": 1,
+        "question": "What does friendship with God involve?",
+        "options": ["Speaking to Him", "Listening to Him", "Drinking coffee together", "Trusting Him"],
+        "answer": ["Speaking to Him", "Listening to Him", "Trusting Him"],
+        "correctMessage": "That's right. Although I guess if God is everywhere, maybe you can drink coffee together."
+    },
+    {
+        "moduleName": "God our friend",
+        "questionNo": 2,
+        "question": "What did St. Augustine say which conveys our need for God?",
+        "options": ["I need God like I need my morning coffee!", "Our heart is restless until it rests in you"],
+        "answer": ["Our heart is restless until it rests in you"],
+        "correctMessage": "That's right."
+    },
+    {
+        "moduleName": "God our friend",
+        "questionNo": 3,
+        "question": "What does the apostle Peter exhort believers to do at the end of 2 Peter?",
+        "options": ["Be generous to your neighbours", "Turn from idols", "Grow in the grace and knowledge of Jesus"],
+        "answer": ["Grow in the grace and knowledge of Jesus"],
+        "correctMessage": "Yes, being God's friend involves growing to be more like Jesus."
+    }
+] 
