013a4debb7da459dee6e140d71fefe395ac109ee
pipwitheridge <phillipwitheridge@gmail.com>
2024-03-14 21:07:59 +1100

    Added 'Your Story' sections

--- quizQuestions.js
+++ quizQuestions.js
@@ -2,6 +2,7 @@ var quizQuestions = [
     {
         "moduleName": "God our maker",
         "questionNo": 1,
+        "questionType": "multiple choice",
         "question": "What do the first few chapters of Genesis aim to give us primarily?",
         "options": ["A theological account of God and people", "A scientific treatise of the origins of matter", "An historical recount of the first humans"],
         "answer": ["A theological account of God and people"],
@@ -10,6 +11,7 @@ var quizQuestions = [
     {
         "moduleName": "God our maker",
         "questionNo": 2,
+        "questionType": "multiple choice",
         "question": "What words accurately describe the way in which God creates in the Bible?",
         "options": ["Lovingly", "Recklessly", "Powerfully", "Randomly"],
         "answer": ["Lovingly", "Powerfully"],
@@ -18,6 +20,7 @@ var quizQuestions = [
     {
         "moduleName": "God our maker",
         "questionNo": 3,
+        "questionType": "multiple choice",
         "question": "Which example was given to show how God's creative activity is used within evangelism in the New Testament?",
         "options": ["Peter's speech in Acts 2", "Paul's speech in Acts 17", "The opening of John's gospel"],
         "answer": ["Paul's speech in Acts 17"],
@@ -26,6 +29,7 @@ var quizQuestions = [
     {
         "moduleName": "God our enemy",
         "questionNo": 1,
+        "questionType": "multiple choice",
         "question": "How is sin described in the Bible?",
         "options": ["Not loving others as we ought", "Not loving God as we ought", "Not doing the good we know we ought to do", "A universal problem"],
         "answer": ["Not loving others as we ought", "Not loving God as we ought", "Not doing the good we know we ought to do", "A universal problem"],
@@ -34,6 +38,7 @@ var quizQuestions = [
     {
         "moduleName": "God our enemy",
         "questionNo": 2,
+        "questionType": "multiple choice",
         "question": "What does sin lead to?",
         "options": ["Forgiveness", "Death", "Wealth", "Separation from God"],
         "answer": ["Death", "Separation from God"],
@@ -42,6 +47,7 @@ var quizQuestions = [
     {
         "moduleName": "God our enemy",
         "questionNo": 3,
+        "questionType": "multiple choice",
         "question": "What image does C.S. Lewis use to describe sinners?",
         "options": ["Children in debt to their parents", "Rebels who must lay down their arms", "Dogs stuck in a great storm"],
         "answer": ["Rebels who must lay down their arms"],
@@ -50,6 +56,7 @@ var quizQuestions = [
     {
         "moduleName": "God our saviour",
         "questionNo": 1,
+        "questionType": "multiple choice",
         "question": "What makes Jesus' our saviour?",
         "options": ["His life", "His death", "His resurrection", "His surname"],
         "answer": ["His life", "His death", "His resurrection"],
@@ -58,6 +65,7 @@ var quizQuestions = [
     {
         "moduleName": "God our saviour",
         "questionNo": 2,
+        "questionType": "multiple choice",
         "question": "How does the Bible describe salvation?",
         "options": ["An undeserved gift", "A one-time offer", "Eternal life"],
         "answer": ["An undeserved gift", "Eternal life"],
@@ -66,6 +74,7 @@ var quizQuestions = [
     {
         "moduleName": "God our saviour",
         "questionNo": 3,
+        "questionType": "multiple choice",
         "question": "According to Jonathan Edwards, what do we contribute to our salvation?",
         "options": ["Ongoing allegiance to Christ", "The sin that made it necessary", "Financial generosity"],
         "answer": ["The sin that made it necessary"],
@@ -74,6 +83,7 @@ var quizQuestions = [
     {
         "moduleName": "God our saviour",
         "questionNo": 4,
+        "questionType": "multiple choice",
         "question": "What does salvation lead to?",
         "options": ["A new life in the here and now", "Eternal life", "Complacency"],
         "answer": ["A new life in the here and now", "Eternal life"],
@@ -82,6 +92,7 @@ var quizQuestions = [
     {
         "moduleName": "God our friend",
         "questionNo": 1,
+        "questionType": "multiple choice",
         "question": "What does friendship with God involve?",
         "options": ["Speaking to Him", "Listening to Him", "Drinking coffee together", "Trusting Him"],
         "answer": ["Speaking to Him", "Listening to Him", "Trusting Him"],
@@ -90,6 +101,7 @@ var quizQuestions = [
     {
         "moduleName": "God our friend",
         "questionNo": 2,
+        "questionType": "multiple choice",
         "question": "What did St. Augustine say which conveys our need for God?",
         "options": ["I need God like I need my morning coffee!", "Our heart is restless until it rests in you"],
         "answer": ["Our heart is restless until it rests in you"],
@@ -98,9 +110,31 @@ var quizQuestions = [
     {
         "moduleName": "God our friend",
         "questionNo": 3,
+        "questionType": "multiple choice",
         "question": "What does the apostle Peter exhort believers to do at the end of 2 Peter?",
         "options": ["Be generous to your neighbours", "Turn from idols", "Grow in the grace and knowledge of Jesus"],
         "answer": ["Grow in the grace and knowledge of Jesus"],
         "correctMessage": "Yes, being God's friend involves growing to be more like Jesus."
-    }
+    },
+    {
+        "moduleName": "Your story beginning",
+        "questionNo": 1,
+        "questionType": "personal response",
+        "question": "Write a couple of sentences on your life before you became a Christian. What did you believe? What did you value in life? How did you live?",
+        "localStorageName": "yourStoryBeginning"
+    },
+    {
+        "moduleName": "Your story middle",
+        "questionNo": 1,
+        "questionType": "personal response",
+        "question": "Write a couple of sentences on what happened to help you become a Christian. What were the key moments, verses, people, experiences? What did you come to believe about Jesus?",
+        "localStorageName": "yourStoryMiddle"
+    },
+    {
+        "moduleName": "Your story end",
+        "questionNo": 1,
+        "questionType": "personal response",
+        "question": "Write a couple of sentences on how becoming a Christian changed your life afterwards. What changed in the way you think and act? What changed about what you value in life?",
+        "localStorageName": "yourStoryEnd"
+    },
 ] 
